6453da60fce219ab4f0f6b0ead99f2638cd944c0
yonny dominguez <yonnydominguez22@gmail.com>
2022-06-17 14:11:34 -0400

    commit inicial

---
+++ src/main/java/GUI/Casilla.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Casilla extends JPanel{
+    
+    private JLabel etq_cas;
+    private int dim_cas, id_cas;
+    
+    public Casilla(){
+        new JPanel();
+    }
+}
---
+++ src/main/java/iphord/horse/Horse.java
@@ -0,0 +1,13 @@
+package iphord.horse;
+import GUI.Frame;
+import GUI.Ventana;
+
+public class Horse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Ventana app = new Ventana();
+        Frame f1 = new Frame();
+    }
+}
